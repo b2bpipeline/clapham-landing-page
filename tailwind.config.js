a40556981d329221653e29a16b49eba5c6fbb783
Jossia Elang <81104672+nexonmegaxus@users.noreply.github.com>
2024-06-12 12:11:13 +0700

    First Commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    colors: {
+      black: "#1F2122",
+      teal: "#90C8AF",
+      green: "#4CA76C",
+      white: "#FFFFFF",
+      blue: "#025985",
+      grey: "#AAADB1",
+      "light-grey": "#F6F7F8",
+    },
+    fontFamily: {
+      manrope: ["Manrope", "sans-serif"],
+    },
+  },
+  plugins: [],
+};
